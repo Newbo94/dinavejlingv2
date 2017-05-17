be5bd07677a10f313a579189eaad5d64d58d11a5
Newbo94 <jannilas@hotmail.de>
2017-05-17 13:36:57 +0200

    v3

--- header.php
+++ header.php
@@ -20,24 +20,24 @@
 		
 		<div class="container">
 		
-			<div class="row">
-				
-				<div class="col-xs-12">
-					
-					<nav class="navbar navbar-default">
-					  <div class="container-fluid">
-					    <!-- Brand and toggle get grouped for better mobile display -->
-					    <div class="navbar-header">
-					      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					        <span class="sr-only">Toggle navigation</span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-					      </button>
-					      <a class="navbar-brand" href="#">Dina Vejling</a>
-					    </div>
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<?php 
+		<nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Dina Vejling</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#"></a>
+     
+			    <?php 
 								wp_nav_menu(array(
 									'theme_location' => 'primary',
 									'container' => false,
@@ -45,12 +45,14 @@
 									)
 								);
 							?>
-						</div>
-					  </div><!-- /.container-fluid -->
-					</nav>
-				
-				</div>
+  
+  </li>
+  
+    </ul>
+  
+   
+  </div><!-- /.navbar-collapse -->
+</nav>
 				
 			</div>
 			
-			<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
--- page-home.php
+++ page-home.php
@@ -3,38 +3,62 @@
  /*
 	Template Name: Home
 */
-    
+
     ?>
     
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    
+    
+    
+    
+    <div class="container">
+    <div class="row">
+        <div class="colum-lg-12">
+    
+    
+    <img src="" alt="">
+    
+    
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
   <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
   </ol>
-  <div class="carousel-inner" role="listbox">
-    <div class="carousel-item active">
-      <img class="d-block img-fluid" src="https://assets.entrepreneur.com/content/16x9/822/1413823428-amazingly-free-stock-websites.jpg" alt="First slide">
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="    https://assets.entrepreneur.com/content/16x9/822/1413823428-amazingly-free-stock-websites.jpg
+" alt="Los Angeles">
     </div>
-    <div class="carousel-item">
-      <img class="d-block img-fluid" src="https://assets.entrepreneur.com/content/16x9/822/1413823428-amazingly-free-stock-websites.jpg" alt="Second slide">
+
+    <div class="item">
+      <img src="    https://assets.entrepreneur.com/content/16x9/822/1413823428-amazingly-free-stock-websites.jpg
+" alt="Chicago">
     </div>
-    <div class="carousel-item">
-      <img class="d-block img-fluid" src="https://assets.entrepreneur.com/content/16x9/822/1413823428-amazingly-free-stock-websites.jpg" alt="Third slide">
+
+    <div class="item">
+      <img src="  https://assets.entrepreneur.com/content/16x9/822/1413823428-amazingly-free-stock-websites.jpg
+" alt="New York">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
     <span class="sr-only">Next</span>
   </a>
+</div
+    
+    </div>
+        </div>
 </div>
-    
-    
-    
+   
    
   
  
