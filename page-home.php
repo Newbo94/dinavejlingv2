bf4ace1c0b5c80b97903822c559fab9f4a387c21
Newbo94 <jannilas@hotmail.de>
2017-05-24 13:16:31 +0200

    wordpress

--- page-home.php
+++ page-home.php
@@ -18,7 +18,7 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
 
-  <!-- Wrapper for slides -->
+  <!-- Wrapper til slides -->
   <div class="carousel-inner">
     <div class="item active">
       <img src="   <?php the_field('image_slider_1'); ?>
@@ -43,35 +43,69 @@
 
 </div> <!----- container-fluid poistion-re------>
 
-<div class="container position-ab">
+
+
+<!-- Social media og kontakt info -->
+
+<div class="container position-ab position-top">
+<div class="row">
+<div class="col-sm-offset-10 col-sm-4">
+<div class="front_social_media">
+<h3> <?php  the_field('mail');?> </h3>
+<h3> <?php  the_field('telefon_');?>  </h3>
+
+
+  <a href="https://www.facebook.com/Dina-Vejling-Dansk-kunsth%C3%A5ndv%C3%A6rk-361328902422/" target="_blank"> <img  class="front_social_media_img" src="<?php the_field('social_media_link_1'); ?>">  </a>
+  <a href="https://www.instagram.com/dinavejling/" target="_blank"><img  class="front_social_media_img"src="<?php the_field('social_media_link_2'); ?>" > </a>
+
+</div>
+</div>
+</div>
+
+
+</div>
+
+
+
+
+<!-- Main logo poisition ---->
+
+<div class="container position-ab position-top">
   <div class="row">
     <div class="col-sm-offset-2 col-md-6">
 <div class="main-logo">
-<img src="<?php the_field('home_header_logo'); ?>" alt="dina vejlign logo ">
+<img src="<?php the_field('home_header_logo'); ?>" alt="dina vejling logo ">
 </div>
 </div>
 </div>
-</div> <!---- Container position-ab ----->
+</div>
 
 
-<div class="container position-ab">
+
+<!-- Adresse og åbningstider info ---->
+
+<div class="container position-ab position-bottom ">
 
 <div class="adress_text row">
 
-<div class="col-sm-offset-2 col-md-2">
+<div class="col-sm-offset-2 col-md-3">
 
 <h3> <?php the_field('adresse_textbox'); ?> </h3>
 
 
+</div>
 
+
+<div class="col-sm-offset-1 col-md-3">
+<h3> <?php the_field('abningstider_textbox'); ?> </h3>
+</div>
+</div>
 </div>
 
 
 </div>
+
 </div>
-
-
-
 
 
 
